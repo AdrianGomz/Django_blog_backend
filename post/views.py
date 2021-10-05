05f0e508f9383c4bc68756b56cfe155925cfe209
isaac <isaac>
2021-10-05 15:38:22 -0600

    Created views for the post app and added urls.py in the post app and updated the main urls.py

--- post/views.py
+++ post/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class PostList(APIView):
+    def get(self,request):
+        postList = Post.objects.all()
+        serializer = PostSerializer(postList,many=True)
+        return Response(serializer.data)
+    def post(self,request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
